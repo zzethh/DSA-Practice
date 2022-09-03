4393a543e67bdd91068abd4c35e9b0ed16ed6428
Zeth <zenithbe7@gmail.com>
2022-09-03 09:19:06 +0530

    added

---
+++ binary trees/height-balanced-tree-concept.cpp
@@ -0,0 +1,168 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class node {
+public:
+	int data;
+	node* left;
+	node* right;
+	node(int data) {
+		this->data = data;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+
+// void preOrder(node* root) {
+// 	if (root == NULL) {
+// 		return;
+// 	}
+// 	cout << root->data << " ";
+// 	preOrder(root->left);
+// 	preOrder(root->right);
+// }
+
+// void inOrder(node* root) {
+// 	if (root == NULL) {
+// 		return;
+// 	}
+// 	inOrder(root->left);
+// 	cout << root->data << " ";
+// 	inOrder(root->right);
+// }
+
+// void postOrder(node* root) {
+// 	if (root == NULL) {
+// 		return;
+// 	}
+// 	postOrder(root->left);
+// 	postOrder(root->right);
+// 	cout << root->data << " ";
+// }
+
+// int search(vector<int> inorder, int start, int end, int curr) {
+// 	for (int i = start; i <= end; i++) {
+// 		if (inorder[i] == curr) {
+// 			return i;
+// 		}
+// 	}
+// 	return -1;
+// }
+
+// node* buildTree(vector<int> postorder, vector<int> inorder, int start, int end) {
+// 	static int i = 4;
+// 	if (start > end) {
+// 		return NULL;
+// 	}
+// 	int curr = postorder[i];
+// 	i--;
+// 	node* n = new node(curr);
+// 	if (start == end) {
+// 		return n;
+// 	}
+// 	int pos = search(inorder, start, end, curr);
+// 	n->right = buildTree(postorder, inorder, pos + 1, end);
+// 	n->left = buildTree(postorder, inorder, start, pos - 1);
+// 	return n;
+// }
+
+
+void levelOrder(node* root) {
+	queue<node*> q;
+	q.push(root);
+	q.push(NULL);
+	while (!q.empty()) {
+		auto element = q.front();
+		q.pop();
+		if (element == NULL and q.empty() == false) {
+			cout << endl;
+			q.push(NULL);
+			continue;
+		}
+		if (element == NULL and q.empty() == true) {
+			return;
+		}
+		cout << element->data << " ";
+		if (element->left != NULL) {
+			q.push(element->left);
+		}
+		if (element->right != NULL) {
+			q.push(element->right);
+		}
+	}
+}
+
+
+// node* buildTree() {
+// 	int d;
+// 	cin >> d;
+// 	if (d == -1) {
+// 		return NULL;
+// 	}
+// 	node* n = new node(d);
+// 	n->left = buildTree();
+// 	n->right = buildTree();
+// 	return n;
+// }
+
+node* levelOrderBuild() {
+	int d;
+	cin >> d;
+	node* root = new node(d);
+	queue<node*> q;
+	q.push(root);
+	while (!q.empty()) {
+		node* current = q.front();
+		q.pop();
+		int c1, c2;
+		cin >> c1 >> c2;
+		if (c1 != -1) {
+			current->left = new node(c1);
+			q.push(current->left);
+		}
+		if (c2 != -1) {
+			current->right = new node(c2);
+			q.push(current->right);
+		}
+	}
+	return root;
+}
+
+class pairxx {
+public:
+	bool isbalanced;
+	int height;
+};
+
+pairxx heightbalanced(node* root) {
+	pairxx p;
+	if (root == NULL) {
+		p.height = 0;
+		p.isbalanced = true;
+		return p;
+	}
+	auto l = heightbalanced(root->left);
+	auto r = heightbalanced(root->right);
+	p.height = max(l.height, r.height) + 1;
+	if (abs(l.height - r.height) <= 1 and l.isbalanced == true and r.isbalanced == true) {
+		p.isbalanced = true;
+	} else {
+		p.isbalanced = false;
+	}
+	return p;
+}
+
+
+
+int main() {
+	node* root = levelOrderBuild();
+	levelOrder(root);
+	pairxx p = heightbalanced(root);
+	cout << endl;
+	cout << p.isbalanced << "x" << p.height;
+
+	return 0;
+}
+
