606980de04c823c4eb259cf87c95000d24477086
Zeth <zenithbe7@gmail.com>
2022-09-22 16:25:45 +0530

    added

---
+++ hashing/valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        //  map<vector<int>,int> m;
+        vector<int> s1(26, 0);
+        vector<int> s2(26, 0);
+        for (char c : s) {
+            s1[c - 'a']++;
+        }
+        for (char c : t) {
+            s2[c - 'a']++;
+        }
+        if (s1 == s2) return true;
+        return false;
+    }
+};
