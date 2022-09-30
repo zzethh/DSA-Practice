bf272e8802d001aa485ed7d73e1b673430d02b35
Zeth <zenithbe7@gmail.com>
2022-09-30 21:14:02 +0530

    added

---
+++ hashing/unique-email-addresses.cpp
@@ -0,0 +1,12 @@
+unordered_set<string> st;
+for (string &email : emails) {
+    string cleanEmail;
+    for (char c : email) {
+        if (c == '+' || c == '@') break;
+        if (c == '.') continue;
+        cleanEmail += c;
+    }
+    cleanEmail += email.substr(email.find('@'));
+    st.insert(cleanEmail);
+}
+return st.size();
