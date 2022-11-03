c913354bb1f7e9d1fcae39a6bb6f8f8d9477b88b
Zeth <zenithbe7@gmail.com>
2022-11-03 14:49:30 +0530

    added

--- greedy/maximum-meetings-in-one-room.cpp
+++ greedy/maximum-meetings-in-one-room.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-        bool static cmp(vector<int> a, vector<int> b) {
-                if (a[1] == b[1]) return a[2] < b[2];
-                return a[1] < b[1];
+        bool static cmp(pair<pair<int, int>, int> a, pair<pair<int, int>, int> b) {
+                if (a.first.second == b.first.second) return a.second < b.second;
+                return a.first.second < b.first.second;
         }
 
         vector<int> maxMeetings(int N, vector<int> &S, vector<int> &F) {
-                vector<vector<int>> v;
+                vector<pair<pair<int, int>, int>> v;
                 for (int i = 0; i < N; i++) {
-                        v.push_back({S[i], F[i], i + 1});
+                        v.push_back({{S[i], F[i]}, i + 1});
                 }
 
                 sort(v.begin(), v.end(), cmp);
 
                 int lowest = 0;
                 vector<int> ans;
-                ans.push_back(v[0][2]);
+                ans.push_back(v[0].second);
                 for (int i = 1; i < N; i++) {
-                        if (v[lowest][1] < v[i][0]) {
-                                ans.push_back(v[i][2]);
+                        if (v[lowest].first.second < v[i].first.first) {
+                                ans.push_back(v[i].second);
                                 lowest = i;
                         }
                 }
