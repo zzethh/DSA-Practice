6dca691012ce57bb073f3c0387e66a23512b230e
Zeth <zenithbe7@gmail.com>
2022-11-27 10:30:12 +0530

    added

---
+++ greedy/queue-reconstruction-by-height.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool static cmp(vector<int> a, vector<int> b) {
+        if (a[0] == b[0]) {
+            return a[1] < b[1];
+        }
+        return a[0] > b[0];
+    }
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), cmp);
+        vector<vector<int>> ans;
+
+        for (auto x : people) {
+            ans.insert(ans.begin() + x[1], x);
+        }
+        return ans;
+    }
+};
