a0195cc69c4864dc7cee12272f459bf20f07287e
Zeth <zenithbe7@gmail.com>
2022-12-27 11:57:58 +0530

    added

---
+++ codeforces contest/div 3/A.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+using ll = long long;
+
+#define pb push_back
+#define z(a) (int)a.size()
+#define forn(i,n) for(int i=0;i<n;i++)
+#define all(v) v.begin(), v.end()
+
+
+
+signed main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		int a = 0, b = 0;
+		char x;
+		cin >> a >> x >> b;
+		cout << a + b;
+		cout << endl;
+	}
+	return 0;
+
+}
