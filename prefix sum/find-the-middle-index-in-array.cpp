f3f3829736f0d883551f335ebb6115b2d877a42f
Zeth <zenithbe7@gmail.com>
2023-02-09 18:36:28 +0530

    added

---
+++ prefix sum/find-the-middle-index-in-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        vector<int> pref;
+        int sum = 0;
+        for (auto x : nums) {
+            sum += x;
+            pref.push_back(sum);
+        }
+        for (int j = 0; j < nums.size(); j++) {
+            if (j == 0) {
+                if (pref[nums.size() - 1] - pref[0] == 0) {
+                    return 0;
+                }
+            }
+            else if (j == nums.size() - 1) {
+                if (pref[j - 1] == 0) {
+                    return j;
+                }
+            }
+            else {
+                if (pref[j - 1] == pref[nums.size() - 1] - pref[j]) {
+                    return j;
+                }
+            }
+        }
+        return -1;
+    }
+};
