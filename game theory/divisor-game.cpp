1a35956d764188b6370e0585f0b3af3276345f34
Zeth <zenithbe7@gmail.com>
2023-02-21 21:28:46 +0530

    Added

--- game theory/divisor-game.cpp
+++ game theory/divisor-game.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
     bool helper(int n, bool x) {
-        ///if(n==1) return false;
+        if (n == 1) return false;
         if (n == 2) return true;
         for (int i = 1; i < n; i++) {
             if (n % i == 0) {
                 bool sub = helper(n - i, !x);
                 if (!sub and x == 1) return true;
-                if (sub and x == 0) return true;
+                if (sub) return true;
             }
         }
         /// if(x==0) return true;
         return false;
     }
+    int dp[1001];
+    bool helper2(int n) {
+        if (n == 1) return false;
+        if (dp[n] != -1) return dp[n];
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                if (helper2(n - i) == 0) return dp[n] = 1;
+            }
+        }
+        return dp[n] = 0;
+    }
     bool divisorGame(int n) {
         // helper(n);
         ///if(n%2==0) return true;
-        return helper(n, 1);
+        memset(dp, -1, sizeof dp);
+        return helper2(n);
     }
 };
