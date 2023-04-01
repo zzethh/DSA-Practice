f25840b5f10cf3f7ece0a8715c6d4b8559b9e090
Zeth <zenithbe7@gmail.com>
2023-04-01 10:57:01 +0530

    added

---
+++ dp/DP leetcode/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int helper(vector<int>& p, bool sold, int i, vector<vector<int>> &dp) {
+        if (i == p.size()) {
+            return 0;
+        }
+        if (dp[i][sold] != -1) return dp[i][sold];
+        int profit = 0;
+        int exc = 0;
+        int exc2 = 0;
+        if (sold) {
+            profit = helper(p, false, i + 1, dp) - p[i];
+            exc = helper(p, true, i + 1, dp);
+        }
+        else {
+            profit = helper(p, true, i + 1, dp) + p[i];
+            exc2 = helper(p, false, i + 1, dp);
+        }
+        return dp[i][sold] = max(profit, max(exc, exc2));
+    }
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
+        return helper(prices, true, 0, dp);
+    }
+};
