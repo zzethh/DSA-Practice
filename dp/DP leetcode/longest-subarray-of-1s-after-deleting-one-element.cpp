575f01288b224d1f01bef6db9743b8d98b676e60
Zeth <zenithbe7@gmail.com>
2023-04-12 19:43:42 +0530

    Added

--- dp/DP leetcode/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ dp/DP leetcode/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,56 +1,20 @@
 class Solution {
 public:
-    int longestSubarray(vector<int>& nums) {
-        int maxi = 0;
-        if (nums.size() == 0) return 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int j = i;
-            int cnt = 0;
-            if (nums[i] == 0) {
-                if (j < nums.size()) j++;
-                while (j < nums.size() and nums[j] == 1) {
-                    j++;
-                    cnt++;
-                }
+    int longestSubarray(vector<int>& A) {
+        int tail = 0;
+        int cnt = 0;
+        int maxi = INT_MIN;
+        for (int i = 0; i < A.size(); i++) {
+            if (A[i] == 0) cnt++;
+            while (cnt > 1) {
+                if (A[tail] == 0) cnt--;
+                tail++;
             }
-            else {
-                while (j < nums.size() and  nums[j] == 1) {
-                    cnt++;
-                    j++;
-                }
-                if (j == nums.size()) {
-                    cnt--;
-                    maxi = max(maxi, cnt);
-                    break;
-                }
-                if (j < nums.size()) j++;
-                while (j < nums.size() and nums[j] == 1) {
-                    j++;
-                    cnt++;
-                }
-            }
-            maxi = max(maxi, cnt);
+            maxi = max(maxi, i - tail);
         }
+
         return maxi;
     }
 };
 
-class Solution {
-public:
-    int longestSubarray(vector<int>& A) {
-        int i = 0, j, k = 1, res = 0;
-        for (j = 0; j < A.size(); ++j) {
-            if (A[j] == 0) {
-                k--;
-            }
-            while (k < 0) {
-                if (A[i] == 0) {
-                    k++;
-                }
-                i++;
-            }
-            res = max(res, j - i);
-        }
-        return res;
-    }
-};
+//https://www.youtube.com/watch?v=Jzy1COg8a34&ab_channel=CodingSamurai%27s
