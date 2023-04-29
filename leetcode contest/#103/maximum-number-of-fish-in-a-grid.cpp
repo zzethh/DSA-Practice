093c3629e710044c559010723de4e3f1291d3e7c
Zeth <zenithbe7@gmail.com>
2023-04-30 00:42:51 +0530

    added

---
+++ leetcode contest/#103/maximum-number-of-fish-in-a-grid.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+//     int helper(vector<vector<int>>& grid,int i,int j,vector<vector<bool>> &v){
+//         if(i>=grid.size() or j>=grid[0].size() or i==-1 or j==-1) return 0;
+//         if(grid[i][j]==0) return 0;
+//         if(v[i][j]) return 0;
+
+//         v[i][j]=1;
+//         int down=helper(grid,i+1,j,v);
+//         int up=helper(grid,i-1,j,v);
+//         int right=helper(grid,i,j+1,v);
+//         int left=helper(grid,i,j-1,v);
+//         v[i][j]=0;
+
+//         return grid[i][j]+max(max(left,right),max(up,down));
+//     }
+
+    int helper(vector<vector<int>>& grid, int i, int j) {
+        if (i >= grid.size() or j >= grid[0].size() or i == -1 or j == -1) return 0;
+        if (grid[i][j] == 0) return 0;
+
+        int temp = grid[i][j];
+        grid[i][j] = 0;
+        int down = helper(grid, i + 1, j);
+        int up = helper(grid, i - 1, j);
+        int right = helper(grid, i, j + 1);
+        int left = helper(grid, i, j - 1);
+        // v[i][j]=0;
+
+        return left + right + down + up + temp;
+        // return temp+max(max(left,right),max(up,down));
+    }
+
+    int findMaxFish(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int maxi = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] > 0) {
+                    // vector<vector<bool>> v(m,vector<bool>(n,0));
+                    maxi = max(maxi, helper(grid, i, j));
+                }
+            }
+        }
+        //vector<vector<bool>> v(m,vector<bool>(n,0));
+        ///return helper(grid,0,0,v);
+        return maxi;
+    }
+};
